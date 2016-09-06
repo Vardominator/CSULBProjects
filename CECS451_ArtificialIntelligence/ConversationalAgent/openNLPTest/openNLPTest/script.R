eecee3179cf4833b8b778b2929b8ba35c6b0a2f8
Vardominator <barsegyanvarderes@gmail.com>
2016-09-06 07:46:11 -0700

    started NLP project

---
+++ CECS451_ArtificialIntelligence/ConversationalAgent/openNLPTest/openNLPTest/script.R
@@ -0,0 +1,17 @@
+require("NLP")
+
+s <- paste(c("Pierre Vinken, 61 years old, will join the board as a ",
+        "nonexecutive director Nov. 29.\n",
+        "Mr. Vinken is chairman of Elsevier N.V., ",
+        "the Dutch publishing group."),
+    collapse = "")
+s <- as.String(s)
+
+
+sent_token_annotator <- Maxent_Sent_Token_Annotator()
+word_token_annotator <- Maxent_Word_Token_Annotator()
+pos_tag_annotator <- Maxent_POS_Tag_Annotator()
+a3 <- annotate(s, list(sent_token_annotator, word_token_annotator, pos_tag_annotator))
+a3
+annotate(s, Maxent_Chunk_Annotator(), a3)
+annotate(s, Maxent_Chunk_Annotator(probs = TRUE), a3)
