2c6cdf2ffaa327ae76d512b3ee5dba7b76cf926e
Vardominator <barsegyanvarderes@gmail.com>
2016-09-09 09:53:03 -0700

    Cleaned up R script. Finished first iteration of the project

--- CECS451_ArtificialIntelligence/ConversationalAgent/FrontEnd/FrontEnd/MainWindow.xaml.cs
+++ CECS451_ArtificialIntelligence/ConversationalAgent/FrontEnd/FrontEnd/MainWindow.xaml.cs
@@ -16,7 +16,7 @@ using System.Windows.Shapes;
 namespace FrontEnd
 {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    /// Interaction logic for IntelAgent
     /// </summary>
     public partial class MainWindow : Window
     {
@@ -29,6 +29,18 @@ namespace FrontEnd
         string rScriptPath = @"F:\GitHub\CSULBProjects\CECS451_ArtificialIntelligence\ConversationalAgent\openNLPTest\openNLPTest\entityTest.R";
         string rScriptResult = string.Empty;
 
+        string[] speechTolkens = new string[4];
+
+        bool gotName = false;
+        bool gotPlaceOfOrigin = false;
+        bool gotDestination = false;
+        bool gotDate = false;
+
+        string placeOfOrigin = string.Empty;
+        string destination = string.Empty;
+        string date = string.Empty;
+        string flightSearch = string.Empty;
+
         public MainWindow()
         {
 
@@ -36,20 +48,122 @@ namespace FrontEnd
             conversationBox.Items.Add($"{intellia}: Welcome! My name Intellia.\nTell me a little about yourself :).");
 
         }
-
         private void userInputTextBox_KeyDown(object sender, KeyEventArgs e)
         {
-            if(e.Key == Key.Enter)
+            if (e.Key == Key.Enter)
             {
-
-                conversationBox.Items.Add($"{user}: {userInputTextBox.Text}");
-               
                 rScriptResult = RScriptHandler.RunCommand(rScriptPath, rScriptExecutablePath, userInputTextBox.Text);
-                conversationBox.Items.Add(rScriptResult);
 
-                userInputTextBox.Clear();
+                Console.WriteLine(rScriptResult);
+
+                speechTolkens = rScriptResult.Split('\r');
+                speechTolkens.Select(tolken => tolken.Trim(new char[] { '\r', '\n' }));
+
+                if (gotName == false)
+                {
+                    if (speechTolkens[0] != "character(0)")
+                    {
+                        user = speechTolkens[0];
+                        conversationBox.Items.Add($"{user}: {userInputTextBox.Text}");
+                        gotName = true;
+                        conversationBox.Items.Add($"{intellia}: Hey {speechTolkens[0]}, welcome! Where are you from?");
+                    }
+                    else
+                    {
+                        conversationBox.Items.Add($"{intellia}: I'm sorry, I did not quite catch that.\nDo you have a name?");
+                    }
+
+                    userInputTextBox.Clear();
+                    return;
+
+                }
+
+                if (gotName == true && gotPlaceOfOrigin == false)
+                {
+                    if (speechTolkens[1] != "character(0)")
+                    {
+                        conversationBox.Items.Add($"{user}: {userInputTextBox.Text}");
+                        conversationBox.Items.Add($"{intellia}: {speechTolkens[1].Trim('\n')} is nice, but everyone needs to\ntravel every now and then.");
+                        conversationBox.Items.Add($"{intellia}: Is there somewhere you would like to visit?");
+                        placeOfOrigin = speechTolkens[1];
+                    }
+                    else
+                    {
+                        conversationBox.Items.Add($"{intellia}: I don't know where that is actually,\nbut I bet you need a serious getaway!");
+                    }
+
+                    gotPlaceOfOrigin = true;
+
+                    userInputTextBox.Clear();
+                    return;
+
+                }
+                
+
+                if(gotPlaceOfOrigin == true && gotDestination == false)
+                {
+
+                    if(speechTolkens[1] != "character(0)")
+                    {
+                        conversationBox.Items.Add($"{user}: {userInputTextBox.Text}");
+                        conversationBox.Items.Add($"{intellia}: {speechTolkens[1].Trim('\n')} it is. I'll see what I could find!");
+                        conversationBox.Items.Add($"{intellia}: Do you have a date for me?");
+                        gotDestination = true;
+                        destination = speechTolkens[1];
+                    }
+                    else
+                    {
+                        conversationBox.Items.Add($"{intellia}: Try again :(. I don't think that place exists.");
+                    }
+
+                    userInputTextBox.Clear();
+                    return;
+
+                }
+
+
+                if(gotDestination == true && gotDate == false)
+                {
+                    if(speechTolkens[3] != "character(0)")
+                    {
+                        conversationBox.Items.Add($"{user}: {userInputTextBox.Text}");
+                        conversationBox.Items.Add($"{intellia}: Give me a sec. I am searching some flights!");
+                        gotDate = true;
+                        date = speechTolkens[3];
+
+                        SetUpFlight(placeOfOrigin, destination, date);
+
+                    }
+                    else
+                    {
+                        conversationBox.Items.Add($"{intellia}: Okay stop messing around. Can you give me a real date?");
+                    }
+
+                    userInputTextBox.Clear();
+                    return;
+
+                }
 
             }
+        }
+
+        public void SetUpFlight(string placeOfOrigin, string destination, string date)
+        {
+
+            string[] splitDate = date.Split(' ');
+            string finalDate = string.Empty;
+
+            for (int i = 0; i < splitDate.Length; i++)
+            {
+                finalDate += splitDate[i] + "+";
+            }
+
+            finalDate.Trim('+');
+
+            string googleSearch = $"{placeOfOrigin}+{destination}+{finalDate}";
+
+            System.Diagnostics.Process.Start("http://google.com/search?q=" + googleSearch + "&ie=utf-8&oe=utf-8");
+
         }
 
     }
--- CECS451_ArtificialIntelligence/ConversationalAgent/openNLPTest/openNLPTest/entityTest.R
+++ CECS451_ArtificialIntelligence/ConversationalAgent/openNLPTest/openNLPTest/entityTest.R
@@ -1,31 +1,43 @@
-# read arguments
+# Uses the 'openNLP' library to perform natural language processing
+#   on an input text.  The 'openNLP' library requires the 'rJava' and
+#   'NLP' libraries to execute all of the necessary functions.
+# 
+# Args: 
+#   x: Any string of characters
+#  
+# Returns: 
+#    A list of persons, locations, organizations, and dates within that text.
+#
+
+
+# read arguments and initialize java access
 args <- commandArgs(TRUE)
-
-
-print(paste(args, collapse = ' ', sep = " "))
-
 options(java.parameters = "- Xmx1024m")
 
+# load necessary libraries
 library(rJava)
 library(NLP)
 library(openNLP)
 
+# combine all input arguments into a single string
 text <- as.String(paste(args, collapse = ' ', sep = " "));
 
+# set up annotator methods
 word_ann <- Maxent_Word_Token_Annotator();
 sent_ann <- Maxent_Sent_Token_Annotator();
 pos_ann <- Maxent_POS_Tag_Annotator();
 
+# perform annotations
 pos_annotations <- annotate(text, list(sent_ann, word_ann, pos_ann));
 text_annotations <- annotate(text, list(sent_ann, word_ann));
 
-text_doc <- AnnotatedPlainTextDocument(text, text_annotations);
-
+# set up methods for finding entitities within the annotations
 person_ann <- Maxent_Entity_Annotator(kind = "person");
 location_ann <- Maxent_Entity_Annotator(kind = "location");
 organization_ann <- Maxent_Entity_Annotator(kind = "organization");
 date_ann <- Maxent_Entity_Annotator(kind = "date");
 
+# combine all extraction methods into a single pipeline
 pipeline <- list(sent_ann,
                  word_ann,
                  person_ann,
@@ -33,9 +45,14 @@ pipeline <- list(sent_ann,
                  organization_ann,
                  date_ann);
 
+
+# run methods on input text
 text_annotations <- annotate(text, pipeline);
 text_doc <- AnnotatedPlainTextDocument(text, text_annotations);
 
+# use this method to extract the lists of entities
+# verbose: if you pass in the category it will return only entities
+#   from that category
 entities <- function(doc, kind) {
     s <- doc$content;
     a <- annotations(doc)[[1]]
@@ -47,7 +64,7 @@ entities <- function(doc, kind) {
     }
 }
 
-
+# print the results into the stream
 print(entities(text_doc, kind = "person"));
 print(entities(text_doc, kind = "location"));
 print(entities(text_doc, kind = "organization"));
