ede5ac1a6821f58b8b558e3cf3f68a2f819e1b8a
Vardominator <barsegyanvarderes@gmail.com>
2016-09-20 01:13:00 -0700

    started 328 first project

---
+++ CECS328_DataStructuresAlgorithms/Project1/Project1/Project1/Program.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project1
+{
+    /// <summary>
+    /// CECS 328 Programming Assignment 1
+    /// 
+    /// Author: Varderes Barsegyan
+    /// Due date: 9/29/2016
+    /// </summary>
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            #region Problem 1: Calculate S(n) by calculating the values of the Fibonacci sequence recursively
+            // S(n) definition: S(n) = f(0) + f(1) +...+ f(n)
+
+            int sampleN = 20;
+            Console.WriteLine($"Problem 1 Result: {FibSum(sampleN)}\n\n");
+            #endregion
+
+            #region Problem 2: Write a non-recursive program to calculate S(n)
+            int first = 0;
+            int second = 1;
+
+            int sum = 1;
+
+            Console.WriteLine("Problem 2 Result: \n");
+            Console.WriteLine($"Fibonacci Table for n = {sampleN}:");
+            Console.WriteLine("f(0) = 0");
+            Console.WriteLine("f(1) = 1");
+            
+            for (int i = 2; i <= sampleN; i++)
+            {
+                int currentFib = first + second;
+                Console.WriteLine($"f({i}) = {currentFib}");
+                sum += currentFib;
+                first = second;
+                second = currentFib;
+            }
+            Console.WriteLine("------------");
+            Console.WriteLine($"Sum: {sum}");
+
+            #endregion
+
+        }
+
+        public static int FibSum(int number)
+        {
+            int result = 0;
+
+            for (int i = 0; i <= number; i++)
+            {
+                result += Fibonacci(i);
+            }
+
+            return result;
+        }
+       
+        public static int Fibonacci(int number)
+        {
+
+            if(number == 0)
+            {
+                return 0;
+            }
+            else if(number == 1)
+            {
+                return 1;
+            }
+
+            return Fibonacci(number - 1) + Fibonacci(number - 2);
+
+        }
+    }
+}
