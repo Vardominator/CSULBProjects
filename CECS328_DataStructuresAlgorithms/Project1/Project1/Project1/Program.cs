9bf026f279b84f84b2b53a6b3f5cf1f0ef971bff
Vardominator <barsegyanvarderes@gmail.com>
2016-09-20 17:35:52 -0700

    finished half of 328 programming assignment

--- CECS328_DataStructuresAlgorithms/Project1/Project1/Project1/Program.cs
+++ CECS328_DataStructuresAlgorithms/Project1/Project1/Project1/Program.cs
@@ -22,7 +22,7 @@ namespace Project1
             // S(n) definition: S(n) = f(0) + f(1) +...+ f(n)
 
             int sampleN = 20;
-            Console.WriteLine($"Problem 1 Result: {FibSum(sampleN)}\n\n");
+            Console.WriteLine($"Problem 1 Result: {Sn(sampleN)}\n\n");
             #endregion
 
             #region Problem 2: Write a non-recursive program to calculate S(n)
@@ -45,13 +45,78 @@ namespace Project1
                 second = currentFib;
             }
             Console.WriteLine("------------");
-            Console.WriteLine($"Sum: {sum}");
+            Console.WriteLine($"Sum: {sum}\n\n");
+            #endregion
+
+            #region Problem 4: Third possible way to sum Fibonnaci
+            Console.WriteLine($"Problem 4: {SnAlt(20)}\n\n");
+            #endregion
+
+            #region Problem 5: Calculate S for n = 10, 20, 30; Calculate f for n = 12, 22, 32
+
+            Console.WriteLine($"Problem 5 results: \n");
+            Console.WriteLine($"S(n) for n = 10 => {SnAlt(10)}");
+            Console.WriteLine($"S(n) for n = 20 => {SnAlt(20)}");
+            Console.WriteLine($"S(n) for n = 30 => {SnAlt(30)}\n");
+            Console.WriteLine($"f(n) for n = 12 => {Fibonacci(12)}");
+            Console.WriteLine($"f(n) for n = 22 => {Fibonacci(22)}");
+            Console.WriteLine($"f(n) for n = 32 => {Fibonacci(32)}\n");
+
+            #endregion
+
+            #region Problem 7: Fourth way to calcualte S(n)
+
+            Console.WriteLine($"Problem 7: {SnAlt2(20)}\n\n");
+
+            #endregion
+
+            #region Problem 8: Largest n that can be computed successfully by each program
+            // Considering that we are using 32-bit integers, there is a limitation on how large n can be
+            //  I will throw an exception to indicate when the resulting integer overflows
+
+            int n = 1;
+
+            while (true)
+            {
+                int result = 0;
+                try
+                {
+
+                }
+                catch (OverflowException exc)
+                {
+                    Console.WriteLine($"Maximum n reached: {n}");
+                    
+                }
+            }
 
             #endregion
 
         }
 
-        public static int FibSum(int number)
+        public static int SnAlt2(int number)
+        {
+            return Fibonacci(number + 2) - 1;
+        }
+
+        public static int SnAlt(int number)
+        {
+            int result = 0;
+
+            for (int i = 0; i <= number; i++)
+            {
+                result += Gn(i);
+            }
+
+            return result;
+
+        }
+        public static int Gn(int k)
+        {
+            return (int)((1 / Math.Sqrt(5)) * (Math.Pow(((1 + Math.Sqrt(5)) / 2), k) - Math.Pow(((1 - Math.Sqrt(5)) / 2), k)));
+        }
+
+        public static int Sn(int number)
         {
             int result = 0;
 
