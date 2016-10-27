132fe893f474e2024162d8abab13927ee57191d7
vardominator <barsegyanvarderes@gmail.com>
2016-10-27 13:40:50 -0700

    blah

--- CECS326_OperatingSystems/Assignment3/pellet.c
+++ CECS326_OperatingSystems/Assignment3/pellet.c
@@ -5,6 +5,7 @@
 #include <sys/ipc.h>
 #include <sys/shm.h>
 #include <pthread.h>
+#include <time.h>
 
 
 #define SHSIZE 100
@@ -13,14 +14,21 @@ typedef int bool;
 #define true 1
 #define false 0
 
+void initializePellet(int *row, int width, int height);
+void dropPellet(int *row, int width, int height, int randomColumn);
+
 int main(char argc, char *argv[])
 {
+
+    srand(time(NULL));
 
     int shmid;
     int width = 5, height = 5;
     int total = width * height;
     int i = 0, j = 0;
     int position = 0;
+
+    int r = rand() % width;
 
     key_t key = 9876;
 
@@ -39,39 +47,59 @@ int main(char argc, char *argv[])
         return 1;
     }
 
+
+    initializePellet(row, width, height);
+
+    dropPellet(row, width, height, r);
+
+
+    shmdt(row);
+
+    
+
+    return 0;
+
+}
+
+
+void initializePellet(int *row, int width, int height)
+{
+    int i = 0, j = 0;
+
     // initialize array
     for(i = 0; i < width; i++)
     {
         for(j = 0; j < height; j++)
         {
             // map 2d array to 1d
-            row[i * width + j] = i * width + j;
+            row[i * width + j] = 0;
         }
     }
 
+}
 
-    while(1)
+void dropPellet(int *row, int width, int height, int randomColumn)
+{
+
+    int i = 0, j = 0;
+
+    printf("Waiting to be read--\n");
+    printf("\033[2J");
+    printf("\033[1;1H");
+
+    for(j = 0; j < height; j++)
     {
 
-        printf("Waiting to be read--\n");
         sleep(1);
-        printf("\033[2J");
-        printf("\033[1;1H");
+        // map 2d array to 1d
+        row[height * j + randomColumn] = 2;
 
-        for(i = 0; i < width; i++)
-        {   
-            for(j = 0; j < height; j++)
-            {
-                // map 2d array to 1d
-                row[i * width + j] += 1;
-            }
-        }
-
+        row[height * (j - 1) + randomColumn] = 0;
 
     }
 
-    shmdt(row);
+    sleep(1);
 
-    return 0;
+    return NULL;
 
 }
