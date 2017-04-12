8d371503175a345903ea8b91452bf4188485c706
Varderes Barsegyan <barsegyanvarderes@gmail.com>
2017-04-11 20:35:58 -0700

    asdfas

--- CECS551_AdvancedAI/ProgrammingAssignments/Assignment4/CECS551_Assignment4/CECS551_Assignment4/script.R
+++ CECS551_AdvancedAI/ProgrammingAssignments/Assignment4/CECS551_Assignment4/CECS551_Assignment4/script.R
@@ -64,17 +64,39 @@ f8 <- list(c(10), c(11))
 f9 <- list(c(12), c(13))
 
 
+
 classifier_bounds <- list(f1, f2, f3, f4, f5, f6, f7, f8, f9)
 
 binary_classifier_table <- data.frame("Description" = character(), "Dataset Size" = integer(), "Degree" = numeric(), "Cost" = numeric(),
                                       "Average CV Accuracy" = numeric(), "Best Accuracy" = numeric(), stringsAsFactors = FALSE)
 
 
-
 for (bound in classifier_bounds) {
     binary_classifier_table[nrow(binary_classifier_table) + 1,] <- 
         binary_classifier(abalone_df, degrees, costs, bound[[1]], bound[[2]])
+    
 }
+print(binary_classifier_table)
+
+
+if (n >= 10) {
+    if (n <= 7) {
+        if (n <= 5) {
+            # class is 5
+        } else if(n == 6 || n == 7) {
+            # class is 6 or 7
+        }
+    } else if (n == 8 || n == 9) {
+            # class is 8 or 9
+    }
+} else if (n <= 10) {
+    if (n == 10 || n == 11) {
+        # class is 10 or 11
+    } else if (n >= 12) {
+
+    }
+}
+
 
 # exercise 2: best learning-parameter(BLP) with binary classifiers
 binary_classifier <- function(df, degrees, costs, negative_class, positive_class) {
@@ -82,15 +104,44 @@ binary_classifier <- function(df, degrees, costs, negative_class, positive_class
     #   the size of the subset, degree, cost, average CV accuracy of the BLP combinations,
     #   and the best accuracy
 
+    description <- ""
 
+    if (length(negative_class) > 2) {
+        description <- paste(description, "<= ", negative_class[length(negative_class)])
+    } else if (length(negative_class) == 2) {
+        description <- paste(description, negative_class[1], "-", negative_class[2])
+    } else {
+        description <- paste(description, negative_class[1])
+    }
+    description <- paste(description, " vs ")
+    if (length(positive_class) > 2) {
+        description <- paste(description, ">= ", positive_class[length(positive_class)])
+    } else if (length(positive_class) == 2) {
+        description <- paste(description, positive_class[1], "-", positive_class[2])
+    } else {
+        description <- paste(description, positive_class[1])
+    }
+
+    df <- df[df$Rings %in% negative_class | df$Rings %in% positive_class,]
+
+    df$Rings[df$Rings %in% negative_class] <- -1
+    df$Rings[df$Rings %in% positive_class] <- 1
+
+    average_accuracy <- 0
     best_accuracy <- 0
     best_d <- 0
     best_c <- 0
+    cross_fold <- 5
+    num_of_combos <- length(costs) * length(degrees)
 
     for (d in degrees) {
         for (c in costs) {
 
-            current_model <- svm(Rings ~ ., data = abalone_df, kernel = "polynomial", degree = d, type = "C-classification", cost = c, cross = cross_fold)
+            current_model <- svm(Rings ~ ., data = df, kernel = "polynomial", degree = d, type = "C-classification", cost = c, cross = cross_fold)
+            average_accuracy = average_accuracy + current_model$tot.accuracy
+
+            predictions <- predict(model, df[, - length(df)])
+            accuracy <- mean(predictions == df[, length(df)])
 
             if (current_model$tot.accuracy > best_accuracy) {
                 best_accuracy <- current_model$tot.accuracy
@@ -100,4 +151,7 @@ binary_classifier <- function(df, degrees, costs, negative_class, positive_class
 
         }
     }
+
+    return(c(description, nrow(df), best_d, best_c, average_accuracy / num_of_combos, best_accuracy))
+
 }
